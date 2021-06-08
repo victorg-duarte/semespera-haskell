c72bf0a6a789882cef1891df9a4d1c1abbdcc3b0
Victor Duarte <vitugduarte@gmail.com>
2021-06-08 19:02:23 -0300

    mensa

--- src/Handler/Calculos.hs
+++ src/Handler/Calculos.hs
@@ -11,9 +11,9 @@ import Import
 -- http://localhost:8080/soma/n1/4/n2/7
 -- mostra 11
 getSomaR :: Int -> Int -> Handler Html
-getSomaR n3 n2 = do
+getSomaR n3 n21 = do
     defaultLayout $ do
-        res <- return (n3+n2)
+        res <- return (n3+n21)
         [whamlet|
             <h1>
                 A some é: #{res}
